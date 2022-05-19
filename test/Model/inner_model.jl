f4fa323ee3319d330ecfbd4d227bf52cdbf4f45b
TomaszWasTaken <71557832+TomaszWasTaken@users.noreply.github.com>
2022-05-19 16:28:10 +0200

    Add files via upload

---
+++ test/Model/inner_model.jl
@@ -0,0 +1,5 @@
+using MySDPSolver, Test, LinearAlgebra, SparseArrays
+
+@testset "InnerModel" begin
+    @test true
+end
---
+++ test/Model/problem_scaling.jl
@@ -0,0 +1,5 @@
+using MySDPSolver, Test, LinearAlgebra, SparseArrays
+
+@testset "ProblemScaling" begin
+    @test true
+end
