42bbc3652678015c755ad83234db5267bf802264
TomaszWasTaken <71557832+TomaszWasTaken@users.noreply.github.com>
2022-05-19 23:01:34 +0200

    Add files via upload

---
+++ src/MySDPSolver.jl
@@ -0,0 +1,38 @@
+module MySDPSolver
+
+using LinearAlgebra, SparseArrays, MKLSparse, SuiteSparse
+using GenericLinearAlgebra
+using Printf
+using MathOptInterface
+
+import LinearAlgebra: BlasInt
+
+import Base.empty!
+
+function version()
+    v"0.0.0"
+end
+
+include("./LinearAlgebra/utils.jl")
+include("./LinearAlgebra/scaling.jl")
+
+#TODO: order matters
+include("./Model/problem_data.jl")
+include("./Model/problem_scaling.jl")
+include("./Model/settings.jl")
+include("./Model/inner_model.jl")
+
+# include("../experimental/cuda_test.jl")
+include("./Model/preprocessing.jl")
+
+include("./LinearAlgebra/workspace.jl")
+include("./IPM/residuals.jl")
+include("./IPM/feasibility.jl")
+include("./IPM/stopping_criteria.jl")
+include("./IPM/predictor_corrector.jl")
+
+include("./LinearAlgebra/decompositions.jl")
+include("./LinearAlgebra/normal_equations.jl")
+
+include("./Interface/MOI_wrapper.jl")
+end # module
